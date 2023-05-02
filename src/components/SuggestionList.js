ba197be8fcce074af71701db6e77134ca9564608
hyukzz <ri_chard@naver.com>
2023-05-03 03:10:37 +0900

    feat: components - 검색창 - 검색어 제안 리스트

---
+++ src/components/SuggestionList.js
@@ -0,0 +1,21 @@
+import React, { useRef } from 'react';
+
+const SuggestionList = ({ suggestions, focusedIndex, setFocusedIndex }) => {
+  const suggestionListRef = useRef(null);
+
+  return (
+    <div ref={suggestionListRef}>
+      {suggestions.map((suggestion, index) => (
+        <li
+          key={index}
+          style={index === focusedIndex ? { backgroundColor: '#ccc' } : {}}
+          onClick={() => setFocusedIndex(index)}
+        >
+          {suggestion.name}
+        </li>
+      ))}
+    </div>
+  );
+};
+
+export default SuggestionList;
