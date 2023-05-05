749b35d71e22391ad8ac222832acdaa837d9724b
Ain Chung <artnouveau0804@gmail.com>
2023-05-05 14:32:14 +0900

    style: 코드 구조 변경
    스타일링을 위한 코드 구조 변경 및 컴포넌트 추가

--- src/components/SearchBarInput.js
+++ src/components/SearchBarInput.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 const SearchBarInput = ({ keyword, handleInputChange, handleKeyDown }) => {
   return (
-    <input
-      className="search-input"
-      type="text"
-      value={keyword}
-      onChange={handleInputChange}
-      onKeyDown={handleKeyDown}
-      placeholder="질환명을 입력해주세요."
-    />
+    <div className="search-form">
+      <input
+        className="search-input"
+        type="text"
+        value={keyword}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        placeholder="질환명을 입력해주세요."
+      />
+      <button className="search-button">
+        <i className="bi bi-search" />
+      </button>
+    </div>
   );
 };
 
