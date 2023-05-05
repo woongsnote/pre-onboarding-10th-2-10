d5edb8073fc23d91232cff8d33446b6cb4a25793
Ain Chung <artnouveau0804@gmail.com>
2023-05-05 13:06:59 +0900

    style: placeholder 변경

--- src/components/SearchBarInput.js
+++ src/components/SearchBarInput.js
@@ -3,11 +3,12 @@ import React from 'react';
 const SearchBarInput = ({ keyword, handleInputChange, handleKeyDown }) => {
   return (
     <input
+      className="search-input"
       type="text"
       value={keyword}
       onChange={handleInputChange}
       onKeyDown={handleKeyDown}
-      placeholder="search"
+      placeholder="질환명을 입력해주세요."
     />
   );
 };
