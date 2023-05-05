0a64f5288b9a23d56b8e7bcac5eb5672d8abd175
Ain Chung <artnouveau0804@gmail.com>
2023-05-05 23:14:57 +0900

    refactor: 추천 검색어 컴포넌트 관련 분리

--- src/components/SuggestionList.js
+++ src/components/SuggestionList.js
@@ -1,31 +1,40 @@
-import React, { useRef } from 'react';
+import { useEffect, useRef } from 'react';
+import SuggestionItem from './SuggestionItem';
 
-const SuggestionList = ({
-  keyword,
-  suggestions,
-  focusedIndex,
-  setFocusedIndex,
-}) => {
+const SuggestionList = ({ suggestions, focusedIndex, setFocusedIndex }) => {
   const suggestionListRef = useRef(null);
+  const MAX_SUGGESTIONS = 7;
+  const startIndex = Math.max(0, focusedIndex - MAX_SUGGESTIONS + 1);
+
+  const renderedSuggestions = suggestions.slice(
+    startIndex,
+    startIndex + MAX_SUGGESTIONS
+  );
+
+  useEffect(() => {
+    setFocusedIndex(-2);
+  }, [suggestions, setFocusedIndex, MAX_SUGGESTIONS]);
 
   return (
     <ul className="suggestion-list" ref={suggestionListRef}>
       <small>추천 검색어</small>
-      {keyword && suggestions.length > 0 ? (
-        suggestions.map((suggestion, index) => (
-          <li
-            key={index}
-            className={`suggestion-item ${
-              index === focusedIndex && `suggestion--focused`
-            }`}
-          >
-            <i className="bi bi-search" />
-            <div onClick={() => setFocusedIndex(index)}>{suggestion.name}</div>
-          </li>
-        ))
-      ) : (
-        <div className="suggestion_no-result">검색어 없음</div>
-      )}
+      {suggestions.length <= MAX_SUGGESTIONS
+        ? suggestions.map((suggestion, index) => (
+            <SuggestionItem
+              suggestion={suggestion}
+              index={index}
+              focusedIndex={focusedIndex}
+              setFocusedIndex={setFocusedIndex}
+            />
+          ))
+        : renderedSuggestions.map((suggestion, index) => (
+            <SuggestionItem
+              suggestion={suggestion}
+              index={startIndex + index}
+              focusedIndex={focusedIndex}
+              setFocusedIndex={setFocusedIndex}
+            />
+          ))}
     </ul>
   );
 };
